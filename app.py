71b9550aff9af308399618fe21391721b43619ae
Vadim Mazurek <asikpro13@mail.ru>
2025-12-12 00:37:21 +0500

    Обновляем порт по документации

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ def main():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, allow_unsafe_werkzeug=True)
+    app.run(host='0.0.0.0', port=10000, debug=True, allow_unsafe_werkzeug=True)
