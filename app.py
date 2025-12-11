e6c48ed35ff8d2d0cef13c1c2f273af4e452ab49
Vadim Mazurek <asikpro13@mail.ru>
2025-12-12 01:08:02 +0500

    тест

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask
-app = Flask(__name__)
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template
+from flask_cors import CORS
+
+app = Flask(__name__, template_folder="templates/web", static_folder="static", static_url_path='')
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Access-Control-Allow-Origin'
+app.config['JSON_AS_ASCII'] = False
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def main():
+    return render_template('index.html')
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=10000, debug=True, allow_unsafe_werkzeug=True)
